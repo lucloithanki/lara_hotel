b3a8ad7609fa3ab3c905c7ed4693691fc48fba2a
haidola <46549236+lucloithanki@users.noreply.github.com>
2020-09-21 11:09:57 +0700

    new project

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
@@ -174,6 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+       
 
     ],
 
@@ -207,7 +209,6 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
-        'Http' => Illuminate\Support\Facades\Http::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
 
     ],
 
---
+++ resources/views/admin/add_product.blade.php
@@ -0,0 +1,90 @@
+@extends('admin_layout')
+@section('admin_content')
+<div class="row">
+            <div class="col-lg-12">
+                    <section class="panel">
+                        <header class="panel-heading">
+                           Thêm sản phẩm
+                        </header>
+                         <?php
+                            $message = Session::get('message');
+                            if($message){
+                                echo '<span class="text-alert">'.$message.'</span>';
+                                Session::put('message',null);
+                            }
+                            ?>
+                        <div class="panel-body">
+
+                            <div class="position-center">
+                                <form role="form" action="{{URL::to('/save-product')}}" method="post" enctype="multipart/form-data">
+                                    {{ csrf_field() }}
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Tên sản phẩm</label>
+                                    <input type="text" name="product_name" class="form-control" id="exampleInputEmail1" placeholder="Tên sản phẩm"
+                                    data-validation="length" data-validation-length="min10" data-validation-error-msg="* Không để trống & > 10 ký tự </br> * Chỉ nhập chữ và số"
+                                    >
+                                    <span></span>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Slug</label>
+                                    <input type="text" name="product_slug" class="form-control" id="exampleInputEmail1" placeholder="Tên danh mục"
+                                    data-validation="length" data-validation-length="2" data-validation-error-msg="*Không được để trống bắt buộc"
+                                    >
+                                </div>
+                                     <div class="form-group">
+                                    <label for="exampleInputEmail1">Giá sản phẩm</label>
+                                    <input type="text" name="product_price" class="form-control" id="exampleInputEmail1" placeholder="Tên danh mục"
+                                    data-validation="number" data-validation-error-msg="* Hãy điền giá sản phẩm </br> * Chỉ sử dụng số" name="product_price"
+                                    >
+                                </div>
+                                  <div class="form-group">
+                                    <label for="exampleInputEmail1">Hình ảnh sản phẩm</label>
+                                    <input type="file" name="product_image" class="form-control" id="exampleInputEmail1">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Mô tả sản phẩm</label>
+                                    <textarea style="resize: none" rows="8" class="form-control" name="product_desc" id="exampleInputPassword1" placeholder="Mô tả sản phẩm"
+                                    data-validation="length" data-validation-length="min200" data-validation-error-msg="Hãy điền mô tả sản phẩm, để thêm ý nghĩa hơn"></textarea>
+                                </div>
+                                 <div class="form-group">
+                                    <label for="exampleInputPassword1">Nội dung sản phẩm</label>
+                                    <textarea style="resize: none" rows="8" class="form-control" name="product_content" id="exampleInputPassword1" placeholder="Nội dung sản phẩm"
+                                    data-validation="length" data-validation-length="min200" data-validation-error-msg="Hãy điền nội dung sản phẩm, để thêm ý nghĩa hơn"
+                                    ></textarea>
+                                </div>
+                                 <div class="form-group">
+                                    <label for="exampleInputPassword1">Danh mục sản phẩm</label>
+                                      <select name="product_cate" class="form-control input-sm m-bot15">
+                                        @foreach($cate_product as $key => $cate)
+                                            <option value="{{$cate->category_id}}">{{$cate->category_name}}</option>
+                                        @endforeach
+                                            
+                                    </select>
+                                </div>
+                                 <div class="form-group">
+                                    <label for="exampleInputPassword1">Thương hiệu</label>
+                                      <select name="product_brand" class="form-control input-sm m-bot15">
+                                        @foreach($brand_product as $key => $brand)
+                                            <option value="{{$brand->brand_id}}">{{$brand->brand_name}}</option>
+                                        @endforeach
+                                            
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Hiển thị</label>
+                                      <select name="product_status" class="form-control input-sm m-bot15">
+                                            <option value="0">Ẩn</option>
+                                            <option value="1">Hiển thị</option>
+                                            
+                                    </select>
+                                </div>
+                               
+                                <button type="submit" name="add_product" class="btn btn-info">Thêm sản phẩm</button>
+                                </form>
+                            </div>
+
+                        </div>
+                    </section>
+
+            </div>
+@endsection
