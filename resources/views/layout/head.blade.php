3de2f367ccfd1c8967a3bfe5561dbcafd087ea68
lucloithanky <hailtph04574@gmail.com>
2020-09-23 15:59:22 +0700

    layout up

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,161 @@
+<!-- Header Section Start -->
+<div class="header-section section">
+
+<!-- Header Top Start -->
+<div class="header-top header-top-one bg-theme-two">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-center">
+
+            <div class="col mt-10 mb-10 d-none d-md-flex">
+                <!-- Header Top Left Start -->
+                <div class="header-top-left">
+                    <p>Chào mừng bạn đến với JASU Baby+Store</p>
+                    <p>Hotline: <a href="tel:0123456789">0123 456 789</a></p>
+                </div><!-- Header Top Left End -->
+            </div>
+
+            <div class="col mt-10 mb-10">
+                <!-- Header Language Currency Start -->
+                <ul class="header-lan-curr">
+
+                    <li><a href="#">VN</a>
+                        <ul>
+                            <li><a href="#">tiếng việt</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="#">VN</a>
+                        <ul>
+                            <li><a href="#">Việt Nam Đồng</a></li>
+                        </ul>
+                    </li>
+
+                </ul><!-- Header Language Currency End -->
+            </div>
+
+            <div class="col mt-10 mb-10">
+                <!-- Header Shop Links Start -->
+                <div class="header-top-right">
+
+                    <p><a href="#">My Account</a></p>
+                    <p><a href="login-register.html">Register</a><a href="login-register.html">Login</a></p>
+
+                </div><!-- Header Shop Links End -->
+            </div>
+
+        </div>
+    </div>
+</div><!-- Header Top End -->
+
+<!-- Header Bottom Start -->
+<div class="header-bottom header-bottom-one header-sticky@">
+    <div class="container-fluid">
+        <div class="row menu-center align-items-center justify-content-between">
+
+            <div class="col mt-15 mb-15">
+                <!-- Logo Start -->
+                <div class="header-logo">
+                    <a href="index.html">
+                        <img src="assets/images/logo.png" alt="Jadusona">
+                    </a>
+                </div><!-- Logo End -->
+            </div>
+
+            <div class="col order-2 order-lg-3">
+                <!-- Header Advance Search Start -->
+                <div class="header-shop-links">
+
+                    <div class="header-search">
+                        <button class="search-toggle"><img src="assets/images/icons/search.png" alt="Search Toggle"><img class="toggle-close" src="assets/images/icons/close.png" alt="Search Toggle"></button>
+                        <div class="header-search-wrap">
+                            <form action="#">
+                                <input type="text" placeholder="Type and hit enter">
+                                <button><img src="assets/images/icons/search.png" alt="Search"></button>
+                            </form>
+                        </div>
+                    </div>
+<!-- 
+                    <div class="header-mini-cart">
+                        <a href="cart.html"><img src="assets/images/icons/cart.png" alt="Cart"> <span>02($250)</span></a>
+                    </div> -->
+
+                </div><!-- Header Advance Search End -->
+            </div>
+
+            <div class="col order-3 order-lg-2">
+                <div class="main-menu">
+                    <nav>
+                        <ul>
+                            <li class="active"><a href="index.html">HOME</a></li>
+                            <li><a href="shop.html">SHOP</a>
+                                <ul class="sub-menu">
+                                    <li><a href="#">Đồ chơi thông minh</a></li>
+                                    <li><a href="#">Đồ chơi vận động</a></li>
+                                    <li><a href="#">Xe đồ chơi trẻ em</a></li>
+                                    <li><a href="#">Đồ dùng cho bé</a></li>
+                                    <li><a href="#">Bể bơi phao bơi</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">PAGES</a>
+                                <ul class="sub-menu">
+                                    <li><a href="cart.html">Cart</a></li>
+                                    <li><a href="checkout.html">Checkout</a></li>
+                                    <li><a href="login-register.html">Login & Register</a></li>
+                                    <li><a href="wishlist.html">Wishlist</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="blog.html">BLOG</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog</a></li>
+                                    <li><a href="single-blog.html">Single Blog</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">CONTACT</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <!-- Mobile Menu -->
+            <div class="mobile-menu order-12 d-block d-lg-none col"></div>
+
+        </div>
+    </div>
+</div><!-- Header BOttom End -->
+
+</div><!-- Header Section End -->
+<!-- Hero Section Start -->
+<div class="hero-section section">
+
+<!-- Hero Slider Start -->
+<div class="hero-slider hero-slider-one fix">
+
+    <!-- Hero Item Start -->
+                        <div class="hero-item" style="background-image: url(assets/images/hero/hero-1.jpg)">
+
+        <!-- Hero Content -->
+        <div class="hero-content">
+
+            <h1>Get 35% off <br>Latest Baby Product</h1>
+            <a href="#">SHOP NOW</a>
+
+        </div>
+
+    </div><!-- Hero Item End -->
+
+    <!-- Hero Item Start -->
+    <div class="hero-item" style="background-image: url({{ asset('assets/images/hero/hero-2.jpg)">
+
+        <!-- Hero Content -->
+        <div class="hero-content">
+
+            <h1>Get 35% off <br>Latest Baby Product</h1>
+            <a href="#">SHOP NOW</a>
+
+        </div>
+
+    </div><!-- Hero Item End -->
+
+</div><!-- Hero Slider End -->
+
+</div><!-- Hero Section End -->
--- resources/views/layout/page.blade.php
+++ resources/views/layout/page.blade.php
@@ -1,24 +1,61 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Home</title>
-<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all" />
-<!--theme-style-->
-<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />	
-<!--//theme-style-->
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--fonts-->
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
-<!--//fonts-->
-<script src="js/jquery.min.js"></script>
-<!--script-->
-</head>
-<body> 
-@include('layout.nav')
-@section('content')
+<!doctype html>
+<html class="no-js') }}" lang="en">
 
-@endsection
-@include('layout.footer')	
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/favicon.ico') }}">
+    
+    <!-- CSS
+	============================================ -->
+   
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/icon-font.min.css') }}">
+    
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}">
+    
+    <!-- Helper CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/helper.css') }}">
+    
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    
+    <!-- Modernizer JS -->
+    <script src="{{ asset('assets/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+</head>
+    @include('layout.head')
+    @section('content')
+    <div class="main-wrapper">
+
+    </div>
+    @endsection
+    @include('layout.footer')
+<body>
+
+<!-- JS
+============================================ -->
+
+<!-- jQuery JS -->
+<script src="{{ asset('assets/js/vendor/jquery-1.12.4.min.js') }}"></script>
+<!-- Popper JS -->
+<script src="{{ asset('assets/js/popper.min.js') }}"></script>
+<!-- Bootstrap JS -->
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<!-- Plugins JS -->
+<script src="{{ asset('assets/js/plugins.js') }}"></script>
+<!-- Ajax Mail -->
+<script src="{{ asset('assets/js/ajax-mail.js') }}"></script>
+<!-- Main JS -->
+<script src="{{ asset('assets/js/main.js') }}"></script>
+
 </body>
+
 </html>
